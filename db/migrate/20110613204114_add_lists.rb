3b44a285c73bccfd3a62586684803969154d4452
Tim Leers <timleers85@gmail.com>
2018-11-13 11:04:49 +0100

    Update urls (#3)
    update doc urls

--- db/migrate/20110613204114_add_lists.rb
+++ db/migrate/20110613204114_add_lists.rb
@@ -11,7 +11,7 @@ class AddLists < ActiveRecord::Migration
 
   def self.up
     list = List.create(:name => 'Welcome')
-    list.tasks.create(:name => 'Check out our docs https://PhraseApp.com/doc')
+    list.tasks.create(:name => 'Check out our docs https://help.phraseapp.com/')
     list.tasks.create(:name => 'Rock on!')
   end
 
